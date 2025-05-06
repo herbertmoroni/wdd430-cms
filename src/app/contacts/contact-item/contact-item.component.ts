94739b6e6de27a60326601ac5987ed0e6ca30e1d
herbertmoroni <herbertmoroni@hotmail.com>
2025-05-05 20:08:26 -0600

    changes to contact

--- src/app/contacts/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-item',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-item.component.css']
 })
 export class ContactItemComponent implements OnInit {
+  @Input() contact: Contact;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
     new Contact(
       '1',
@@ -29,5 +31,9 @@ export class ContactListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
   }
 }
